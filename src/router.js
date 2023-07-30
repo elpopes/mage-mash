62bb816d60e97bf696b14ee9cd2b1c5c7d24f907
lorenzo <lorenzotijerina@gmail.com>
2023-07-30 00:07:08 -0400

    add router.js

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "hash",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HelloWorld,
+    },
+    // Additional routes go here
+  ],
+});
