896acce15dd29deb3321a794fa69c5eeffc52997
lorenzo <lorenzotijerina@gmail.com>
2023-07-30 10:07:44 -0400

    add HomePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="home">
+        <div class="banner">MAGE<br>MASH</div>
+        <button class="start-btn">Start</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+
+<style scoped>
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    text-align: center;
+    font-family: 'Bungee Spice', sans-serif;
+}
+
+.banner {
+    font-size: 5vw; /* This will resize the font based on the width of the viewport */
+}
+
+.start-btn {
+    margin-top: 20px;
+    padding: 10px 20px;
+    font-size: 24px;
+    cursor: pointer;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import HomePage from "./components/HomePage.vue";
 
 Vue.use(Router);
 
@@ -11,7 +11,7 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: HelloWorld,
+      component: HomePage,
     },
     // Additional routes go here
   ],
