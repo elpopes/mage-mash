4e8b09baaf0ce27b20ae351915e22c02c1e93516
lorenzo <lorenzotijerina@gmail.com>
2023-08-02 00:50:37 -0400

    create selection screen

---
+++ src/components/SelectionScreen.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <Draggable v-model="cards" class="bench">
+        <div v-for="card in cards" :key="card">
+          <component :is="card" />
+        </div>
+      </Draggable>
+      <div class="selection-area">
+        <!-- The player's selections -->
+      </div>
+      <button class="ready-button">Ready!</button>
+    </div>
+</template>
+  
+
+<script>
+import Draggable from 'vuedraggable';
+
+import GreenPanda from './GreenPanda.vue';
+import BlueBird from './BlueBird.vue';
+import PurpleSnake from './PurpleSnake.vue';
+
+export default {
+  name: 'SelectionScreen',
+  components: {
+    Draggable,
+    GreenPanda,
+    BlueBird,
+    PurpleSnake
+  },
+  data() {
+    return {
+      cards: ['GreenPanda', 'BlueBird', 'PurpleSnake'],
+    };
+  }
+};
+</script>
