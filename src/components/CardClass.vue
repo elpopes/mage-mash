1b9c9605f530aa32fdfc96f8b31e5aeed2b04ed7
lorenzo <lorenzotijerina@gmail.com>
2023-08-02 23:13:53 -0400

    add card data to SelectionScreen

--- src/components/CardClass.vue
+++ src/components/CardClass.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card">
+    <div class="card" @click="handleClick">
       <h2>{{ name }}</h2>
       <ul>
         <li>Type: {{ type }}</li>
@@ -11,23 +11,29 @@
         </li>
       </ul>
     </div>
-</template>
-  
-<script>
-export default {
+  </template>
+    
+  <script>
+  export default {
     name: 'CardClass',
     props: {
-        name: String,
-        type: String,
-        hp: Number,
-        moves: Array
+      name: String,
+      type: String,
+      hp: Number,
+      moves: Array
+    },
+    methods: {
+      handleClick() {
+        this.$emit('card-clicked', this.name);
+      }
     }
-}
-</script>
-  
-<style scoped>
-.card {
+  }
+  </script>
+    
+  <style scoped>
+  .card {
     border: 1px solid #000;
     padding: 10px;
-}
-</style>
+  }
+  </style>
+  
--- src/components/SelectionScreen.vue
+++ src/components/SelectionScreen.vue
@@ -1,69 +1,108 @@
 <template>
-    <div class="card" @click="handleClick">
-      <h2>{{ name }}</h2>
-      <ul>
-        <li>Type: {{ type }}</li>
-        <li>HP: {{ hp }}</li>
-        <li>Moves: 
-          <ul>
-            <li v-for="move in moves" :key="move.name">{{ move.name }}</li>
-          </ul>
-        </li>
-      </ul>
+    <div>
+      <div class="bench">
+        <CardClass
+          v-for="card in bench"
+          :key="card.name"
+          :name="card.name"
+          :type="card.type"
+          :hp="card.hp"
+          :moves="card.moves"
+        />
+      </div>
+      <div class="selection-area">
+        <CardClass
+          v-for="card in cards"
+          :key="card.name"
+          @card-clicked="selectCard"
+          :name="card.name"
+          :type="card.type"
+          :hp="card.hp"
+          :moves="card.moves"
+        />
+      </div>
+      <button class="ready-button">Ready!</button>
     </div>
   </template>
-    
+  
   <script>
+  import CardClass from './CardClass.vue';
+  
   export default {
-    name: 'CardClass',
-    props: {
-      name: String,
-      type: String,
-      hp: Number,
-      moves: Array
+    name: 'SelectionScreen',
+    components: {
+      CardClass,
+    },
+    data() {
+      return {
+        cards: [
+          { 
+            name: 'GreenPanda', 
+            type: 'Earth', 
+            hp: 120, 
+            moves: [
+              { name: 'Heal', type: 'Defense', healing: 20 },
+              { name: 'Rage Attack', type: 'Offense', damage: 'variable' }
+            ]
+          },
+          { 
+            name: 'BlueBird', 
+            type: 'Air', 
+            hp: 80, 
+            moves: [
+              { name: 'Sleep Spell', type: 'Control', effect: 'Sleep' },
+              { name: 'Inspire', type: 'Support', effect: 'Boost' }
+            ]
+          },
+          { 
+            name: 'PurpleSnake', 
+            type: 'Poison', 
+            hp: 100, 
+            moves: [
+              { name: 'Poison Bite', type: 'Offense', damage: 30 },
+              { name: 'Shed Skin', type: 'Defense', healing: 'variable' }
+            ]
+          }
+        ],
+        bench: []
+      };
     },
     methods: {
-      handleClick() {
-        this.$emit('card-clicked', this.name);
+      selectCard(cardName) {
+        const card = this.cards.find(c => c.name === cardName);
+        this.bench.push(card);
+        this.cards = this.cards.filter(c => c.name !== card.name);
       }
     }
-  }
+  };
   </script>
-    
+  
   <style scoped>
-  .card {
-    border: 1px solid #000;
-    padding: 10px;
+  .bench {
+    display: flex;
+    width: 100%;
+    height: 200px;
+    border-radius: 50px;
+    background-color: #ccc;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 30px;
+  }
+  
+  .selection-area {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 200px;
+    margin-bottom: 30px;
+  }
+  
+  .ready-button {
+    display: block;
+    margin: auto;
+    padding: 10px 20px;
+    font-size: 24px;
+    cursor: pointer;
   }
   </style>
   
-
-
-<style scoped>
-.bench {
-  display: flex;
-  width: 100%;
-  height: 200px;
-  border-radius: 50px;
-  background-color: #ccc;
-  justify-content: space-around;
-  align-items: center;
-  margin-bottom: 30px;
-}
-
-.selection-area {
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-  height: 200px;
-  margin-bottom: 30px;
-}
-
-.ready-button {
-  display: block;
-  margin: auto;
-  padding: 10px 20px;
-  font-size: 24px;
-  cursor: pointer;
-}
-</style>
