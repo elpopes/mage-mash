6d6925180668eae4289abde0d657984d6e729802
lorenzo <lorenzotijerina@gmail.com>
2023-08-02 23:23:11 -0400

    styling of cards

--- src/components/CardClass.vue
+++ src/components/CardClass.vue
@@ -30,10 +30,38 @@
   }
   </script>
     
-  <style scoped>
-  .card {
-    border: 1px solid #000;
-    padding: 10px;
-  }
-  </style>
+    <style scoped>
+    .card {
+        border: 1px solid #000;
+        padding: 20px;
+        background-color: white;
+        border-radius: 15px;
+        box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.15);
+        width: 200px; 
+        height: 250px; 
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between; 
+        transition: 0.3s;
+    }
+    
+    .card:hover {
+        transform: scale(1.02); 
+        box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3); 
+    }
+    
+    .card h2 {
+        margin-top: 0;
+    }
+    
+    .card ul {
+        list-style-type: none; 
+        padding: 0; 
+    }
+    
+    .card ul li {
+        padding: 5px 0; 
+    }
+    </style>
+    
   
