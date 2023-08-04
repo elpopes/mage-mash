d855ac7246a2e54a56a7701cb58127180219a23b
lorenzo <lorenzotijerina@gmail.com>
2023-08-04 00:26:23 -0400

    ready light up

--- src/components/SelectionScreen.vue
+++ src/components/SelectionScreen.vue
@@ -1,40 +1,40 @@
-<template>
-    <div>
-      <div class="bench">
-        <CardClass
-          v-for="card in bench"
-          :key="card.name"
-          :name="card.name"
-          :type="card.type"
-          :hp="card.hp"
-          :moves="card.moves"
-        />
-      </div>
-      <div class="selection-area">
-        <CardClass
-          v-for="card in cards"
-          :key="card.name"
-          @card-clicked="selectCard"
-          :name="card.name"
-          :type="card.type"
-          :hp="card.hp"
-          :moves="card.moves"
-        />
-      </div>
-      <button class="ready-button">Ready!</button>
+  <template>
+    <div class="container">
+        <div class="bench">
+            <CardClass
+                v-for="card in bench"
+                :key="card.name"
+                :name="card.name"
+                :type="card.type"
+                :hp="card.hp"
+                :moves="card.moves"
+            />
+        </div>
+        <div class="selection-area">
+            <CardClass
+                v-for="card in cards"
+                :key="card.name"
+                @card-clicked="selectCard"
+                :name="card.name"
+                :type="card.type"
+                :hp="card.hp"
+                :moves="card.moves"
+            />
+        </div>
+        <button class="ready-button" :class="{ 'bench-full': isBenchFull }">Ready!</button>
     </div>
-  </template>
-  
-  <script>
-  import CardClass from './CardClass.vue';
-  
-  export default {
+</template>
+
+<script>
+import CardClass from './CardClass.vue';
+
+export default {
     name: 'SelectionScreen',
     components: {
-      CardClass,
+        CardClass,
     },
     data() {
-      return {
+        return {
         cards: [
           { 
             name: 'GreenPanda', 
@@ -67,42 +67,52 @@
         bench: []
       };
     },
+    computed: {
+        isBenchFull() {
+            return this.bench.length === 3;
+        },
+    },
     methods: {
-      selectCard(cardName) {
-        const card = this.cards.find(c => c.name === cardName);
-        this.bench.push(card);
-        this.cards = this.cards.filter(c => c.name !== card.name);
-      }
+        selectCard(cardName) {
+            const card = this.cards.find(c => c.name === cardName);
+            this.bench.push(card);
+            this.cards = this.cards.filter(c => c.name !== card.name);
+        }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .bench {
+};
+</script>
+
+<style scoped>
+.container {
+    position: relative;
+    height: 100%;
+}
+.bench, .selection-area {
     display: flex;
+    justify-content: space-around;
     width: 100%;
     height: 200px;
+    margin-bottom: 30px;
+}
+.bench {
     border-radius: 50px;
-    background-color: #ccc;
-    justify-content: space-around;
+    background-color: #0b9710;
     align-items: center;
-    margin-bottom: 30px;
-  }
-  
-  .selection-area {
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
-    height: 200px;
-    margin-bottom: 30px;
-  }
-  
-  .ready-button {
+}
+.ready-button {
     display: block;
     margin: auto;
     padding: 10px 20px;
     font-size: 24px;
     cursor: pointer;
-  }
-  </style>
-  
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    transition: background-color 0.3s;
+}
+.bench-full {
+    background-color: lime;
+    color: black;
+}
+</style>
