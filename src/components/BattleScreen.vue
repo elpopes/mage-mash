ba7396e3a6a07b50bb6f3c0a6fd08ef19db72490
lorenzo <lorenzotijerina@gmail.com>
2023-08-05 23:42:27 -0400

    styling battlescreen

--- src/components/BattleScreen.vue
+++ src/components/BattleScreen.vue
@@ -14,7 +14,7 @@
   
   <script>
   import Card from './Card.vue';
-  import { deepClone } from './utils.js';  // Assume deepClone is in a utils file
+  import { deepClone } from './utils.js'; 
   import baddies from './Baddies.js';
   
   export default {
@@ -41,6 +41,41 @@
   </script>
   
   <style scoped>
-  /* Your styling here */
+  .battle-container {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      height: 100vh;
+      background-color: #f3f4f6;
+      padding: 2rem 5rem;
+  }
+  
+  .player-side, .baddie-side {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 2rem;
+      border: 2px solid #e5e7eb;
+      border-radius: 1rem;
+      background-color: #ffffff;
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
+  
+  button {
+      padding: 0.5rem 1rem;
+      font-size: 1rem;
+      background-color: #0b9710;
+      color: #ffffff;
+      border: none;
+      border-radius: 0.5rem;
+      cursor: pointer;
+      transition: background-color 0.3s;
+      margin-top: 2rem;
+  }
+  
+  button:hover {
+      background-color: #088c07;
+  }
   </style>
   
