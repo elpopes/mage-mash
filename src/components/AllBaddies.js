469a4fe832e7363420dda1c9abe395b14bbaba9c
lorenzo <lorenzotijerina@gmail.com>
2023-08-05 23:29:51 -0400

    add baddies

---
+++ src/components/AllBaddies.js
@@ -0,0 +1,31 @@
+export default {
+  level01: [
+    {
+      name: "RedScorpion",
+      type: "Desert",
+      hp: 55,
+      moves: [
+        { name: "Stinger Poke", type: "Offense", damage: 15 },
+        { name: "Sand Cloak", type: "Defense", healing: 10 },
+      ],
+    },
+    {
+      name: "IceWolf",
+      type: "Tundra",
+      hp: 50,
+      moves: [
+        { name: "Chilled Nip", type: "Offense", damage: 10 },
+        { name: "Frosty Fur", type: "Defense", healing: 5 },
+      ],
+    },
+    {
+      name: "MysticBunny",
+      type: "Meadow",
+      hp: 45,
+      moves: [
+        { name: "Hop Kick", type: "Offense", damage: 8 },
+        { name: "Healing Herbs", type: "Defense", healing: 12 },
+      ],
+    },
+  ],
+};
